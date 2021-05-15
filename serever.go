d0bf2750e146e156a29ad7800e1c13a1e3767ab4
aostrovska <anastasija.ostrovska25@gmail.com>
2021-05-15 11:45:02 +0300

    sha

--- serever.go
+++ serever.go
@@ -4,71 +4,80 @@ import (
 	"net/http"
 	"crypto/sha256"
 	"io"
+	"encoding/json"
 	"io/ioutil"
-	"fmt"
+	"time"
 )
 
 type Session struct{
 	Id int
-	UserId int
-	idAdress string 
-	OpenedDate string
-	Duration string
-	ExpirationDate  string
+	UserId string
+	ipAdress string 
+	OpenedDate time.Time
+	Duration int
+	ExpirationDate  time.Time
 }
 
 type logPas struct{
-	Login string `login`
+	Username string `login`
 	Password string `password`
 }
 
-var session []Session 
-var idn int
+var ses Session
+var user = logPas{"username", "password"}
 
 func setupResponse(w *http.ResponseWriter, req *http.Request) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
     (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
     (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+    (*w).Header().Set("Access-Control-Expose-Headers", "Sha")
 }
 
 func Handler(w http.ResponseWriter, req *http.Request) {
 	setupResponse(&w, req)
-	pas := sha256.New()
-	data, err := ioutil.ReadAll(req.Body)
-	req.Body.Close()
-	var person logPas
-	err = json.Unmarshal(data, &person)
-	if err != nil{
-		return err
-	}
-	pas.Write([]byte(person.Password))
-	if (person.Login == "login")&&(pas.Sum(nil) == "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8"){
-		session[len(session)] = {1, 1, "id1029e", "17.04.2021", "3", "18.04.2021"}
-		idn = 1
-	}else{
-		io.WriteString(w, "unsuccessful login")
-	}
-	
-}
-
-func Handler2(w http.ResponseWriter, req *http.Request) {
-	if (correctID(idn)){
-		io.WriteString(w, "you succcessfuly gained data")
-	}else{
-		io.WriteString(w, "you unsucccessfuly gained data")
-	}
-
-
-}
-
-func correctID(idn int) bool{
-	for i := 0; i< len(session); i++{
-		if idn == session[i].Id{
-			return true 
+	if (*req).Method == "OPTIONS" {
+		w.WriteHeader(204)
+	}else if req.Method == "POST" {
+		data, err := ioutil.ReadAll(req.Body)
+		req.Body.Close()
+		var person logPas
+		err = json.Unmarshal(data, &person)
+		if err != nil{
+			return 
+		}
+		if (person.Username == user.Username)&&(person.Password == user.Password){
+			pas := sha256.New()
+			pas.Write([]byte(person.Password))
+			if err != nil{
+				return
+			}
+			w.Header().Set("Sha",string( pas))
+			ses = Session{1,string( pas),req.Header.Get("X-FORWARDED-FOR"), time.Now(),15, time.Now().Local().AddDate(0, 15, 0)}
+		}else {
+			io.WriteString(w, "incorrect login")
 		}
 	}
-	return false
+
 }
+
+
+
+func Handler2(w http.ResponseWriter, req *http.Request) {
+	
+	setupResponse(&w, req)
+	if (*req).Method == "OPTIONS" {
+		w.WriteHeader(204)
+	}else if req.Method == "GET" {
+		if ((*req).Header.Get("Sha") == ses.UserId)&&(time.Now().Before( ses.ExpirationDate)){
+			io.WriteString(w, "you succcessfuly gained data")
+		}else{
+			io.WriteString(w, "401")
+		}
+	}
+
+
+}
+
 	
 
 func main() {
